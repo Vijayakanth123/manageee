d1dc430afca728a59efff6c826cdbf202352615c
vijaya kanth <ch.vijayakanthhere@gmail.com>
2026-07-21 13:32:20 +0530

    started backend and frontend for login and signup

---
+++ backend/models/accountModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        unique : true,
+        required: true
+    },
+
+    password_hashed : {
+        type : String,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model("Account",accountSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+//routes
+const accRoutes = require("./routes/accRoutes");
+
+//starting express instance and check
 const app = express();
-app.use(express.json());
-
-const url = "mongodb://127.0.0.1:27017/manageee";
-
-mongoose.connect(url)
-    .then(() => console.log("Connected Successfully"))
-    .catch((e) => console.log(e));
-
 const PORT = 3000;
-
 app.listen(PORT, () => console.log(`Server running on: ${PORT}`));
+
+//middle-ware
+app.use(express.json());
+app.use(express.static('../frontend')); // automatically serve files from that folder(without this both will run in different ports)
+app.use('/api/acc',accRoutes);
+
+// data-base connect and check
+const url = "mongodb://127.0.0.1:27017/manageee";
+mongoose.connect(url)
+.then(() => console.log("Connected Successfully"))
+.catch((e) => console.log(e));
